41ba4d8fa1b5a25fd49c3746975ff44dfed59de3
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2024-05-08 12:20:49 +0100

    Batch Files

---
+++ pull_all.bat
@@ -0,0 +1,10 @@
+cd libs/zing/libs/zest
+git pull
+git submodule update --init --recursive
+cd ../../
+git pull
+git submodule update --init --recursive
+cd ../../
+git pull
+git submodule update --init --recursive
+
